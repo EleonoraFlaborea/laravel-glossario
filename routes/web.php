78c8b5040acde40ff7a9874e2f6a5cb55c8a355f
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-04-08 10:25:35 +0200

    added routes soft delete

--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', GuestHomeController::class)->name('guest.home');
 Route::get('/words/{word}', [GuestHomeController::class, 'show'])->name('guest.words.show');
 
+// Route x il cestino
+Route::get('/words/trash', [WordController::class, 'trash'])->name('words.trash');
+// Route x recupero dal cestino
+Route::patch('/words/{word}/restore', [WordController::class, 'restore'])->name('words.restore')->withTrashed();
+// Route x eliminazione definitiva
+Route::delete('/words/{word}/drop', [WordController::class, 'drop'])->name('words.drop')->withTrashed();
 
-
-
+// Rotta per la home Admin
 Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
 // Route x Index
 Route::get('/admin/words', [WordController::class, 'index'])->name('admin.words.index');
+// Route x Create
 Route::get('/admin/words/create', [WordController::class, 'create'])->name('admin.words.create');
 // Route x Show
 Route::get('/admin/words/{word}', [WordController::class, 'show'])->name('admin.words.show');
 // Rotta per la modifica della parola
 Route::get('/admin/words/{word}/edit', [WordController::class, 'edit'])->name('admin.words.edit');
-
+// Route per Store
 Route::post('/admin/words', [WordController::class, 'store'])->name('admin.words.store');
+// Route per Delete
 Route::delete('/admin/words/{word}', [WordController::class, 'destroy'])->name('admin.words.destroy');
 // Rotta per salvataggio della modifica su db
 Route::put('/admin/words/{word}/', [WordController::class, 'update'])->name('admin.words.update');
+
+
+
+
 
 
 
