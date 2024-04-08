ddc45223e5a247991a3ee4414f1a9e48e21ef8ff
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-04-08 10:14:13 +0200

    added softDeletes in model Word

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 use Carbon\Carbon;
 
 class Word extends Model
 {
     use HasFactory;
+    use softDeletes;
+
 
     protected $fillable = ['word_name', 'description'];
 
